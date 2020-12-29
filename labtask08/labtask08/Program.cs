eac86c340a851d1d69768520005efb7382285800
Piu Terasa Costa <piucosta98@gail.com>
2020-12-29 11:08:49 +0600

    final term labtasks

---
+++ labtask08/labtask08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator calcu = new calculator();
+            Console.WriteLine(" Sum = " + calcu.sum(10, 5));
+            Console.WriteLine(" Sub = " + calcu.sub(10, 5));
+            Console.WriteLine(" Division = " + calcu.division(25,5));
+            Console.WriteLine(" Multiplication = " + calcu.multiplication(10, 5));
+            Console.WriteLine(" Division = " + calcu.division(10, 5));
+            Console.WriteLine(" Power = " + calcu.XtoY(10, 5));
+            Console.WriteLine(" Factorial = " + calcu.factorial(5));
+
+
+        }
+    }
+}
